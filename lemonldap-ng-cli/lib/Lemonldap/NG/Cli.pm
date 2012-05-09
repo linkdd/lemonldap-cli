f4cefbcaea92cdb9d1f67e61dc09e63cf80a2285
David Delassus <david.jose.delassus@gmail.com>
2012-05-09 17:13:31 +0200

    Add basis of Lemonldap::NG::Cli

---
+++ lemonldap-ng-cli/lib/Lemonldap/NG/Cli.pm
@@ -0,0 +1,38 @@
+package Lemonldap::NG::Cli;
+
+our $VERSION = "0.1";
+
+use Lemonldap::NG::Common::Conf;
+
+use strict;
+1;
+__END__
+
+=head1 NAME
+
+=encoding utf8
+
+Lemonldap::NG::Cli - Command Line Interface to edit LemonLDAP::NG configuration.
+
+=head1 DESCRIPTION
+
+Lemonldap::NG::Cli allow user to edit the configuration of Lemonldap::NG via the
+command line.
+
+=head1 SEE ALSO
+
+L<Lemonldap::NG>, L<Lemonldap::NG::Common::Conf>
+
+=head1 AUTHOR
+
+David Delassus E<lt>david.jose.delassus@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012, by David Delassus
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
